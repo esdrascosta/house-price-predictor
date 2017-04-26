8d6c05aee363f16484617cb500c2d5c2fd482739
esdrascosta <servoesdras@gmail.com>
2017-04-25 23:38:59 -0300

    simple linear regression

--- src/main/scala/LinearRegression.scala
+++ src/main/scala/LinearRegression.scala
@@ -1,26 +1,38 @@
-package br.com.esdras
-
-import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.linalg.{DenseMatrix, DenseVector, sum}
 
 class LinearRegression {
 
   def gradientDescent(X: DenseMatrix[Double],
-                      y: DenseVector[Double],
-                      theta: DenseVector[Double],
+                      y: DenseMatrix[Double],
+                      theta: DenseMatrix[Double],
                       alpha: Double,
-                      numInteractions: Int): (DenseVector[Double], DenseVector[Double]) = {
+                      numInter: Int): (DenseMatrix[Double], DenseVector[Double]) = {
 
+    val costHistory = DenseVector.zeros[Double](numInter)
+    val m = y.size
+    var _theta = theta
 
-    ???
+    for( i <- 0 until numInter) {
+
+      val a = alpha/m
+      val H = X * _theta - y
+      _theta = _theta - ( a * X.t * H )
+
+      costHistory(i) = computeCost(X, y, _theta)
+    }
+
+    (_theta, costHistory)
   }
 
-  def costFunction(X: DenseMatrix[Double],
-                   y: DenseVector[Double],
-                   theta: DenseVector[Double] ) = {
+  def computeCost(X: DenseMatrix[Double],
+                  y: DenseMatrix[Double],
+                  theta: DenseMatrix[Double] ) = {
 
-    val m = y.length
-    val H = (X*theta - y)
-    (1/(2*m)) *  (H.t * H)
+    val m = y.size
+    val H = X * theta - y
+    val b = 1.0 / ( 2.0 * m )
+    val cost: DenseMatrix[Double] = b *  H.t * H
+    sum(cost)
   }
 
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,39 +1,43 @@
-package br.com.esdras
-
-import java.io.File
-
-import breeze.linalg.{DenseMatrix, DenseVector, csvread}
+import breeze.linalg._
+import breeze.math._
+import breeze.numerics._
+import breeze.plot.Figure
+import breeze.plot._
 
 object Main extends App {
 
- val resource = getClass.getResource("boaviagem_dataset.csv").getPath
+  // TODO
+  //  val resource = getClass.getResource("boaviagem_dataset.csv").getPath
+  //  val dataSet = csvread(new File(resource), skipLines = 1)
 
- val dataSet = csvread(new File(resource), skipLines = 1)
+  val X = DenseMatrix(
+    (2.0, 1.0, 3.0),
+    (7.0, 1.0, 9.0),
+    (1.0, 8.0, 1.0),
+    (3.0, 7.0, 4.0)
+  )
 
-// val f = Figure()
-// val p = f.subplot(0)
-// p.title = "exploratory data analysis"
+  val y = DenseMatrix(
+    2.0,
+    5.0,
+    5.0,
+    6.0
+  )
 
- val X: DenseMatrix[Double] = dataSet(::, 1 to 4)
- println(X)
-// val y = dataSet(::, 0)
-//println(y)
-// p += plot(xs,ys, '.')
-// f.refresh()
+  val theta = DenseMatrix(
+    0.0,
+    0.0,
+    0.0
+  )
 
- /*
-  X = [ 2 1 3; 7 1 9; 1 8 1; 3 7 4 ];
-y = [2 ; 5 ; 5 ; 6];
-theta_test = [0.4 ; 0.6 ; 0.8];
-  */
+  val regressionModel = new LinearRegression()
+  val (newTheta, costHistory) = regressionModel.gradientDescent(X, y, theta, 0.01, 20)
 
-//val X = DenseMatrix(
-//  (2.0, 1.0, 3.0),
-//  (7.0, 1.0, 9.0),
-//  (3.0, 7.0, 4.0)
-//)
-//
-//val y = DenseVector()
-//val linear = new LinearRegression()
-//linear.costFunction()
+  val xs = linspace(0, costHistory.length, costHistory.length)
+  val f = Figure()
+  val p = f.subplot(0)
+  p.title = "Cost Function"
+  p += plot(xs, costHistory, '-')
+  f.refresh()
+
 }
